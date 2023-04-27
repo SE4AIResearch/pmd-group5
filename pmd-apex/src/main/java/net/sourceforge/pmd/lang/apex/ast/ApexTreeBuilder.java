4c1a229b6c5fa5e18da28f5c0147ec476e8c5471
owl0695 <124755787+owl0695@users.noreply.github.com>
2023-04-27 14:47:40 -0400

    Update ApexTreeBuilder.java

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ApexTreeBuilder.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ApexTreeBuilder.java
@@ -463,7 +463,6 @@ final class ApexTreeBuilder extends AstVisitor<AdditionalPassScope> {
      * <p>
      * Note that the provided token list must implement {@link RandomAccess}.
      */
-    @Override
     private static final class TokenListByStartIndex extends AbstractList<Integer> implements RandomAccess {
 
         private final List<TokenLocation> tokens;
